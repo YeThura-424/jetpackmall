f31d637864cb58653822eafa72a34b96db86c14b
YeThura-424 <rathanpoudle.26@gmail.com>
2023-10-11 12:12:00 +0630

    category listing

--- resources/views/backend/category/list.blade.php
+++ resources/views/backend/category/list.blade.php
@@ -16,51 +16,52 @@
                     <div class="tile-body">
                         @if(session('successMsg') != NULL)
                         <div class="alert alert-success alert-dismissible fade show" role="alert">
-                          <strong>SUCCESS!</strong> {{session('successMsg')}}
-                          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    @endif
-                    <div class="table-responsive">
-                        <table class="table table-hover table-bordered" id="sampleTable">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Name</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @php $i = 1; @endphp
-                                @foreach($categories as $category)
-                                @php  
-                                $id = $category->id;
-                                $name = $category->name;
-                                $photo = $category->photo;
-                                @endphp
-                                <tr>
-                                    <td>{{$i++}}.</td>
-                                    <td> <img src="{{asset($photo)}}" class="img-fluid" style="width: 90px; object-fit: cover;">
-                                    {{$name}}</td>
-                                    <td>
-                                        <a href="{{route('backside.category.edit',$id)}}" class="btn btn-warning">
-                                            <i class="icofont-ui-settings icofont-1x"></i>
-                                        </a>
-                                        <form action=" {{route('backside.category.destroy',$id)}} " method="POST" class="d-inline-block" onsubmit="return confirm('Are you sure to delete the item?')">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button class="btn btn-outline-danger" type="submit"><i class="icofont-close icofont-1x"></i></button>
-                                        </form>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                            <strong>SUCCESS!</strong> {{session('successMsg')}}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        @endif
+                        <div class="table-responsive">
+                            <table class="table table-hover table-bordered" id="sampleTable">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php $i = 1; @endphp
+                                    @foreach($categories as $category)
+                                    @php
+                                    $id = $category->id;
+                                    $name = $category->name;
+                                    $photo = $category->photo;
+                                    @endphp
+                                    <tr>
+                                        <td>{{$i++}}.</td>
+                                        <td> <img src="{{asset($photo)}}" class="img-fluid" style="width: 90px; object-fit: cover;">
+                                            {{$name}}
+                                        </td>
+                                        <td>
+                                            <a href="{{route('backside.category.edit',$id)}}" class="btn btn-warning">
+                                                <i class="icofont-ui-settings icofont-1x"></i>
+                                            </a>
+                                            <form action=" {{route('backside.category.destroy',$id)}} " method="POST" class="d-inline-block" onsubmit="return confirm('Are you sure to delete the item?')">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="btn btn-outline-danger" type="submit"><i class="icofont-close icofont-1x"></i></button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 </x-backend>
