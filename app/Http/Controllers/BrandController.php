e8567c3f80959e2867559c20a4bc0870602760d1
YeThura-424 <rathanpoudle.26@gmail.com>
2023-10-12 12:12:00 +0630

    brand controller , list view

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -7,103 +7,103 @@ use App\Brand;
 
 class BrandController extends Controller
 {
-        /**
-        * Display a listing of the resource.
-        *
-        * @return \Illuminate\Http\Response
-        */
-        public function index()
-        {
-            $brands = Brand::all();
-            return view('backend.brand.list',compact('brands'));
-        }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $brands = Brand::all();
+        return view('backend.brand.list', compact('brands'));
+    }
 
-     /**
+    /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-     public function create()
-     {
+    public function create()
+    {
         return view('backend.brand.new');
     }
 
     /**
-    * Store a newly created resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @return \Illuminate\Http\Response
-    */
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         $validator = $request->validate([
-            'name' => ['required','string','max:255','unique:brands'],
+            'name' => ['required', 'string', 'max:255', 'unique:brands'],
             'logo' => 'required|mimes:jpeg,bmp,png,jpg'
         ]);
         // dd($request);
         if ($validator) {
             $name = $request->name;
             $photo = $request->logo;
-                    // var_dump($photo);die();
-                    // set image file path
-            $imagename = time().'.'.$photo->extension();
-            $photo->move(public_path('images/brand'),$imagename);
-            $filepath = 'images/brand/'.$imagename;
+            // var_dump($photo);die();
+            // set image file path
+            $imagename = time() . '.' . $photo->extension();
+            $photo->move(public_path('images/brand'), $imagename);
+            $filepath = 'images/brand/' . $imagename;
 
-                    //Data insert
+            //Data insert
             $brand = new Brand();
             $brand->name = $name;
             $brand->logo = $filepath;
             $brand->save();
-            return redirect()->route('backside.brand.index')->with('successMsg','New Brand is ADDED in your data');
+            return redirect()->route('backside.brand.index')->with('successMsg', 'New Brand is ADDED in your data');
         } else {
             return redirect::back()->withErrors($validator);
         }
     }
 
     /**
-    * Display the specified resource.
-    *
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show($id)
     {
         //
     }
 
     /**
-    * Show the form for editing the specified resource.
-    *
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function edit($id)
     {
         // dd($id);
         $brand = Brand::find($id);
-        return view('backend.brand.edit',compact('brand'));
+        return view('backend.brand.edit', compact('brand'));
     }
 
     /**
-    * Update the specified resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, $id)
     {
 
-        
+
         $name = $request->name;
         $newlogo = $request->logo;
         $oldlogo = $request->oldLogo;
 
         if ($request->hasFile('logo')) {
-            $imageName = time().'.'.$newlogo->extension();
-            $newlogo->move(public_path('images/brand'),$imageName);
-            $filepath = 'images/brand/'.$imageName;
+            $imageName = time() . '.' . $newlogo->extension();
+            $newlogo->move(public_path('images/brand'), $imageName);
+            $filepath = 'images/brand/' . $imageName;
             if (\File::exists(public_path($oldlogo))) {
                 \File::delete(public_path($oldlogo));
             }
@@ -111,28 +111,27 @@ class BrandController extends Controller
             $filepath = $oldlogo;
         }
 
-                // data update 
+        // data update
 
         $brand = Brand::find($id);
         $brand->name = $name;
         $brand->logo = $filepath;
         $brand->save();
 
-        return redirect()->route('backside.brand.index')->with('successMsg','Existing Brand is UPDATED in your data');
-        
+        return redirect()->route('backside.brand.index')->with('successMsg', 'Existing Brand is UPDATED in your data');
     }
 
     /**
-    * Remove the specified resource from storage.
-    *
-    * @param  int  $id
-    * @return \Illuminate\Http\Response
-    */
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
         $brand = Brand::find($id);
         // dd($brand);
         $brand->delete();
-        return redirect()->route('backside.brand.index')->with('successMsg','Existing Brand is DELETED in your data');
+        return redirect()->route('backside.brand.index')->with('successMsg', 'Existing Brand is DELETED in your data');
     }
 }
--- resources/views/backend/subcategory/list.blade.php
+++ resources/views/backend/subcategory/list.blade.php
@@ -15,63 +15,64 @@
                 <div class="tile">
                     @if(session('successMsg') != NULL)
                     <div class="alert alert-success alert-dismissible fade show" role="alert">
-                      <strong>SUCCESS!</strong> {{session('successMsg')}}
-                      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                @endif
-                <div class="tile-body">
-                    <div class="table-responsive">
-                        <table class="table table-hover table-bordered" id="sampleTable">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Name</th>
-                                    <th>Category</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @php 
-                                $i = 1;
-                                @endphp
-                                @foreach($subcategories as $subcategory)
-                                @php
-                                $id = $subcategory->id;
-                                $name = $subcategory->name;
-                                $photo = $subcategory->photo;
-                                $categoryid = $subcategory->category_id;
-                                $category = $subcategory->category->name;
-                                @endphp
-                                <tr>
-                                    <td> {{$i++}} </td>
-                                    <td><img src="{{asset($photo)}}" class="img-fluid" style="width: 90px; object-fit: cover;">
-                                         {{$name}} </td>
-                                    <td> {{$category}} </td>
-                                    <td>
-                                        <a href=" {{route('backside.subcategory.edit',$id)}} " class="btn btn-warning">
-                                            <i class="icofont-ui-settings icofont-1x"></i>
-                                        </a>
-                                        <form action="{{ route('backside.subcategory.destroy',$id) }}" method="POST" class="d-inline-block" onsubmit="return confirm('Are you sure?')">
+                        <strong>SUCCESS!</strong> {{session('successMsg')}}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    @endif
+                    <div class="tile-body">
+                        <div class="table-responsive">
+                            <table class="table table-hover table-bordered" id="sampleTable">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name</th>
+                                        <th>Category</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                    $i = 1;
+                                    @endphp
+                                    @foreach($subcategories as $subcategory)
+                                    @php
+                                    $id = $subcategory->id;
+                                    $name = $subcategory->name;
+                                    $photo = $subcategory->photo;
+                                    $categoryid = $subcategory->category_id;
+                                    $category = $subcategory->category->name;
+                                    @endphp
+                                    <tr>
+                                        <td> {{$i++}} </td>
+                                        <td><img src="{{asset($photo)}}" class="img-fluid" style="width: 90px; object-fit: cover;">
+                                            {{$name}}
+                                        </td>
+                                        <td> {{$category}} </td>
+                                        <td>
+                                            <a href=" {{route('backside.subcategory.edit',$id)}} " class="btn btn-warning">
+                                                <i class="icofont-ui-settings icofont-1x"></i>
+                                            </a>
+                                            <form action="{{ route('backside.subcategory.destroy',$id) }}" method="POST" class="d-inline-block" onsubmit="return confirm('Are you sure?')">
 
-                                            @csrf
-                                            @method('DELETE')
+                                                @csrf
+                                                @method('DELETE')
 
-                                            <button class="btn btn-outline-danger" type="submit"> 
-                                                <i class="icofont-close icofont-1x"></i>
-                                            </button>
+                                                <button class="btn btn-outline-danger" type="submit">
+                                                    <i class="icofont-close icofont-1x"></i>
+                                                </button>
 
-                                        </form>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 </x-backend>
