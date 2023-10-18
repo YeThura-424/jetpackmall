28f240bcd58f6cba84658b446172f0d957d1d335
YeThura-424 <rathanpoudle.26@gmail.com>
2023-10-18 12:12:00 +0630

    townsip listing

--- resources/views/backend/township/list.blade.php
+++ resources/views/backend/township/list.blade.php
@@ -15,60 +15,60 @@
                 <div class="tile">
                     @if(session('successMsg') != NULL)
                     <div class="alert alert-success alert-dismissible fade show" role="alert">
-                      <strong>SUCCESS!</strong> {{session('successMsg')}}
-                      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                @endif
-                <div class="tile-body">
-                    <div class="table-responsive">
-                        <table class="table table-hover table-bordered" id="sampleTable">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Name</th>
-                                    <th>Price</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @php 
-                                $i = 1;
-                                @endphp
-                                @foreach($townships as $township)
-                                @php
-                                $name = $township->name;
-                                $price = $township->price;
-                                $id = $township->id;
-                                @endphp
-                                <tr>
-                                    <td> {{$i++}} </td>
-                                    <td> {{$name}} </td>
-                                    <td> {{$price.' MMK'}} </td>
-                                    <td>
-                                        <a href=" {{route('backside.township.edit',$id)}}" class="btn btn-warning">
-                                            <i class="icofont-ui-settings icofont-1x"></i>
-                                        </a>
-                                        <form action="{{ route('backside.township.destroy',$id) }}" method="POST" class="d-inline-block" onsubmit="return confirm('Are you sure?')">
+                        <strong>SUCCESS!</strong> {{session('successMsg')}}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    @endif
+                    <div class="tile-body">
+                        <div class="table-responsive">
+                            <table class="table table-hover table-bordered" id="sampleTable">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name</th>
+                                        <th>Price</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                    $i = 1;
+                                    @endphp
+                                    @foreach($townships as $township)
+                                    @php
+                                    $name = $township->name;
+                                    $price = $township->price;
+                                    $id = $township->id;
+                                    @endphp
+                                    <tr>
+                                        <td> {{$i++}} </td>
+                                        <td> {{$name}} </td>
+                                        <td> {{$price.' MMK'}} </td>
+                                        <td>
+                                            <a href=" {{route('backside.township.edit',$id)}}" class="btn btn-warning">
+                                                <i class="icofont-ui-settings icofont-1x"></i>
+                                            </a>
+                                            <form action="{{ route('backside.township.destroy',$id) }}" method="POST" class="d-inline-block" onsubmit="return confirm('Are you sure?')">
 
-                                            @csrf
-                                            @method('DELETE')
+                                                @csrf
+                                                @method('DELETE')
 
-                                            <button class="btn btn-outline-danger" type="submit"> 
-                                                <i class="icofont-close icofont-1x"></i>
-                                            </button>
+                                                <button class="btn btn-outline-danger" type="submit">
+                                                    <i class="icofont-close icofont-1x"></i>
+                                                </button>
 
-                                        </form>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 </x-backend>
