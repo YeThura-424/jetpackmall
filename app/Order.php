380e4155d44db6d6d8fc2a0b8de17d384a7746db
YeThura-424 <rathanpoudle.26@gmail.com>
2023-10-20 12:12:00 +0630

    order model

--- app/Order.php
+++ app/Order.php
@@ -7,23 +7,24 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-	use SoftDeletes;
-	protected $fillable = [
-		'orderdate','voucherno','total','note','status','user_id'
-	];
+    use SoftDeletes;
+    protected $fillable = [
+        'orderdate',
+        'voucherno',
+        'total',
+        'note',
+        'status',
+        'user_id'
+    ];
 
-	// public function orderdetails()
-	// {
-	// 	return $this->hasMany('App\Orderdetail');
-	// }
-	public function user()
-	{
-		return $this->belongsTo('App\User');
-	}
-	public function items()
-	{
-		return $this->belongsToMany('App\Item','orderdetails','order_id','item_id')
-		->withPivot('qty','price','discount','subtotal')
-		->withTimestamps();;
-	}
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    public function items()
+    {
+        return $this->belongsToMany('App\Item', 'orderdetails', 'order_id', 'item_id')
+            ->withPivot('qty', 'price', 'discount', 'subtotal')
+            ->withTimestamps();;
+    }
 }
