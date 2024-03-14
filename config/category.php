7b861c7bdecc10f100dc3296a5fd1fd05b54b9d7
YeThura-424 <rathanpoudle.26@gmail.com>
2024-03-14 21:46:00 +0630

    category seeder

---
+++ config/category.php
@@ -0,0 +1,43 @@
+<?php
+return [
+    [
+        'name' => 'Electronics',
+        'photo' => 'https://via.placeholder.com/640x480?text=Electronics',
+    ],
+    [
+        'name' => 'Clothing',
+        'photo' => 'https://via.placeholder.com/640x480?text=Clothing',
+    ],
+    [
+        'name' => 'Books',
+        'photo' => 'https://via.placeholder.com/640x480?text=Books',
+    ],
+    [
+        'name' => 'Home & Kitchen',
+        'photo' => 'https://via.placeholder.com/640x480?text=Home+%26+Kitchen',
+    ],
+    [
+        'name' => 'Sports & Outdoors',
+        'photo' => 'https://via.placeholder.com/640x480?text=Sports+%26+Outdoors',
+    ],
+    [
+        'name' => 'Health & Personal Care',
+        'photo' => 'https://via.placeholder.com/640x480?text=Health+%26+Personal+Care',
+    ],
+    [
+        'name' => 'Toys & Games',
+        'photo' => 'https://via.placeholder.com/640x480?text=Toys+%26+Games',
+    ],
+    [
+        'name' => 'Beauty & Personal Care',
+        'photo' => 'https://via.placeholder.com/640x480?text=Beauty+%26+Personal+Care',
+    ],
+    [
+        'name' => 'Automotive',
+        'photo' => 'https://via.placeholder.com/640x480?text=Automotive',
+    ],
+    [
+        'name' => 'Grocery & Gourmet Foods',
+        'photo' => 'https://via.placeholder.com/640x480?text=Grocery+%26+Gourmet+Foods',
+    ],
+];
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,9 +12,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-    	// $this->call(TownshipSeeder::class);
-    	// $this->call(RolesTableSeeder::class);
+    	$this->call(TownshipSeeder::class);
+    	$this->call(RolesTableSeeder::class);
     	$this->call(UsersTableSeeder::class);
+
+        $categories = config('category');
+        echo('Seeding Category');
+        foreach($categories as $category){
+            Category::create([
+                'name' => $category['name'],
+                'photo' => $category['photo'],
+            ]);
+        }
+
+        // $categories = Category::all();
 
     }
 }
