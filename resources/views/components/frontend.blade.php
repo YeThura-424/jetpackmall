d4770df74db4ec6d0f35d2a824c0a8b071d02b2f
YeThura-424 <rathanpoudle.26@gmail.com>
2024-04-05 21:33:58 +0630

    footer

--- resources/views/components/frontend.blade.php
+++ resources/views/components/frontend.blade.php
@@ -319,18 +319,6 @@
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="footer__copyright">
-                        <div class="footer__copyright__text">
-                            <p> Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with by <a href="http://myanmaritc.com/" target="_blank"> MMIT </a> 
-                            </p>
-                        </div>
-
-                        <div class="footer__copyright__payment"><img src="frontend/img/payment-item.png" alt=""></div>
-                    </div>
-                </div>
-            </div>
         </div>
     </footer>
     <!-- Footer Section End -->
